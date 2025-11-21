bc850d008e9d12454b1aa3c85802a99daaa4d6cf
Alex <alexbaturin@list.ru>
2025-11-21 10:07:29 +0800

    Course Work2 begin

---
+++ src/test/java/org/skypro/coursework2/Coursework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.coursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
